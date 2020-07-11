1c452d06f6a3fbba2bbafedd9f0a748c2427e3bb
Renan Maganha <maganha.renan@gmail.com>
2020-07-11 14:03:17 -0300

    Now the app get the public user data when the user account is logged in

--- OnTheMap/Model/ParseAPI/StudentLocation.swift
+++ OnTheMap/Model/ParseAPI/StudentLocation.swift
@@ -40,4 +40,4 @@ struct StudentLocation: Codable, Equatable, Hashable {
 }
 
 
-{\"uniqueKey\": \"1234\", \"firstName\": \"John\", \"lastName\": \"Doe\",\"mapString\": \"Mountain View, CA\", \"mediaURL\": \"https://udacity.com\",\"latitude\": 37.386052, \"longitude\": -122.083851}
+//{\"uniqueKey\": \"1234\", \"firstName\": \"John\", \"lastName\": \"Doe\",\"mapString\": \"Mountain View, CA\", \"mediaURL\": \"https://udacity.com\",\"latitude\": 37.386052, \"longitude\": -122.083851}
